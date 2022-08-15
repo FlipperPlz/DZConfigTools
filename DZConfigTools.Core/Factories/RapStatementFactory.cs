16437bb371cb0a46efc41bb5718046fcf2cfbfd2
Ryann (Flipper) <flipperplz@protonmail.com>
2022-08-15 11:28:15 -0700

    feat(models): Add Class Declaration
    - Added class declaration object.
    - Created statement factory.

---
+++ DZConfigTools.Core/Factories/RapStatementFactory.cs
@@ -0,0 +1,17 @@
+﻿using DZConfigTools.Core.Generated;
+using DZConfigTools.Core.Models.Declarations;
+using DZConfigTools.Core.Models.Statements;
+
+namespace DZConfigTools.Core.Factories; 
+
+public static class RapStatementFactory {
+    public static IRapStatement Create(ParamFileParser.StatementContext ctx) {
+        if (ctx.classDeclaration() is { } @class) return (IRapStatement) new RapClassDeclaration().ReadParseTree(@class);
+        if (ctx.externalClassDeclaration() is { } external) return (IRapStatement) new RapExternalClassStatement().ReadParseTree(external);
+        if (ctx.tokenDeclaration() is { } var) return (IRapStatement) new RapVariableDeclaration().ReadParseTree(var);
+        if (ctx.arrayAppension() is { } appension) return (IRapStatement) new RapAppensionStatement().ReadParseTree(appension);
+        if (ctx.arrayDeclaration() is { } array) return (IRapStatement) new RapArrayDeclaration().ReadParseTree(array);
+        if (ctx.deleteStatement() is { } delete) return (IRapStatement) new RapDeleteStatement().ReadParseTree(delete);
+        throw new NotSupportedException();
+    }
+}
---
+++ DZConfigTools.Core/Models/Declarations/RapClassDeclaration.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using DZConfigTools.Core.Factories;
+using DZConfigTools.Core.Generated;
+using DZConfigTools.Core.IO;
+using DZConfigTools.Core.Models.Statements;
+
+namespace DZConfigTools.Core.Models.Declarations; 
+
+public class RapClassDeclaration : IRapStatement, IRapDeserializable<ParamFileParser.ClassDeclarationContext> {
+    private string Classname { get; set; } = string.Empty;
+    private string? ParentClassname { get; set; } = null;
+    private List<IRapStatement> Statements { get; set; } = new();
+
+    public uint BinaryOffset { get; set; } = 0;
+    public long? BinaryOffsetPosition { get; set; } = null;
+    
+    public void WriteBinarized(BinaryWriter writer) {
+        writer.Write((byte) 0);
+        writer.WriteAsciiZ(Classname);
+        BinaryOffsetPosition = writer.BaseStream.Position;
+        writer.Write((uint) BinaryOffset);
+    }
+
+    public string ToParseTree() {
+        var builder = new StringBuilder("class ").Append(Classname);
+        if (ParentClassname is not null) builder.Append(" : ").Append(ParentClassname);
+        builder.Append(" {\n");
+        Statements.ForEach(s => builder.Append(s.ToParseTree()).Append('\n'));
+        return builder.Append("};").ToString();
+    }
+
+    public IRapDeserializable<ParamFileParser.ClassDeclarationContext> ReadBinarized(BinaryReader reader) {
+        if (reader.ReadByte() != 0) throw new Exception("Expected class.");
+        Classname = reader.ReadAsciiZ();
+        BinaryOffsetPosition = reader.BaseStream.Position;
+        BinaryOffset = reader.ReadUInt32();
+        return this;
+    }
+
+    public IRapDeserializable<ParamFileParser.ClassDeclarationContext> ReadParseTree(ParamFileParser.ClassDeclarationContext ctx) {
+        if (ctx.classname is not { } classname) throw new Exception();
+        Classname = classname.GetText();
+        if (ctx.superclass is { } superclass) ParentClassname = superclass.GetText();
+        if (ctx.statement() is { } statements) Statements.AddRange(statements.Select(RapStatementFactory.Create));
+        return this;
+    }
+}
